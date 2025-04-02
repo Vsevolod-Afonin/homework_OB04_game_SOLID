c4d871dae81587ce31b7c2fde23260454f4af644
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-04-02 19:26:15 +0700

    chut chut

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 
-class Figther():
-    pass
 
-class Monster():
-    pass
 
 
 class Weapon(ABC):
@@ -15,8 +11,22 @@ class Weapon(ABC):
 
 class Sword(Weapon):
     def atach(self):
+        print('Боец выбирает меч.')
         print('Боец наносит удар мечом')
+        print('Монстр побеждён')
 
 class Bow(Weapon):
     def atach(self):
+        print('Боец выбирает лук.')
         print('Боец наносит стреляет из лука')
+        print('Монстр побеждён')
+
+class Figther():
+    def __init__(self, weapon: Weapon):
+        self.weapon = weapon
+
+    def change_weapon(self):
+        print(f'Боец сменил оружие на {self.weapon}')
+
+class Monster():
+    pass
